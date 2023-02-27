79e35237f5275be3e560ca0f92a3d2d9e076e1c3
otiv33 <vito.abeln@gmail.com>
2023-02-27 11:09:04 +0100

    fix

--- AirQualityAPI/app.js
+++ AirQualityAPI/app.js
@@ -3,25 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const http = require('http');
-const https = require('https');
-const fs = require('fs');
 
 // Rpoutes
 var indexRouter = require('./routes/index');
 var airQualityRouter = require('./routes/airQuality');
 
 var app = express();
-
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/20.222.149.200/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/20.222.149.200/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/20.222.149.200/chain.pem', 'utf8');
-const credentials = {
-	key: privateKey,
-	cert: certificate,
-	ca: ca
-};
 
 // Read settings.json
 const fs = require('fs');
@@ -70,12 +57,9 @@ app.use(function(err, req, res, next) {
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
 
-httpServer.listen(8080, () => {
-  console.log(`Server is running on port 8080.`);
-});
-
-httpsServer.listen(443, () => {
-  console.log(`Server is running on port 443.`);
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
 
 module.exports = app;
