a068a15771faf90d9f9226a4e501e6e11f71465c
Vito Abeln <vito.abeln@gmail.com>
2023-04-12 18:07:37 +0200

    Added data cleaning and structuring

--- NARM/main.py
+++ NARM/main.py
@@ -2,15 +2,15 @@ import pandas as pd
 from data_service import data_service
 
 if __name__ == '__main__':
-    # Get data
-    ds = data_service()
-    ds.get_data(load_fresh_data=True)
-    ds.clean_data()
-    data = ds.data
     
-    # Save data to excel
-    data = pd.DataFrame(data)
-    data.to_excel('data/data.xlsx', index=False)
-    print(data)
+    fresh = True
+    
+    ds = data_service(load_fresh_data=fresh)
+    ds.structure_data()
+    ds.calibrate_temperature()
+    ds.clean_data_zero()
+    ds.clean_data_mean()
+    ds.check_for_na_values()
+    
     
     
